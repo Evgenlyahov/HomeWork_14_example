ad2b5106958c6fac5c4b2fe479c863d800fce6a0
Evgen <evgen_lyahov@mail.ru>
2021-11-07 10:04:53 +0300

    Example 1

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум введённым числам проверять является ли первое квадратом второго
+System.Console.Write("Введи первое число: ");
+float number1;
+number1 = float.Parse(Console.ReadLine());
+System.Console.Write("Введи второе число: ");
+float number2;
+number2 = float.Parse(Console.ReadLine());
+if (number1 == number2 * number2)
+System.Console.WriteLine("Первое число является квадратом второго");
+else
+{
+    System.Console.WriteLine("Первое число не является квадратом второго");
+}
