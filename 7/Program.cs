ba53fa77f692a2e70c94e2e22f6526e459990ddf
Evgen <evgen_lyahov@mail.ru>
2021-11-07 13:08:19 +0300

    Example 7

---
+++ 7/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать числа от -N до N
+
+Console.Clear();
+float[] array = new float[21] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+for(int i = 0; i < 21; i++)
+{
+   System.Console.Write(" " + array[i] + " ");
+}
