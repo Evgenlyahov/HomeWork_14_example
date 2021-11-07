1aff69efabad4062f657976c93b9dc229ddffc23
Evgen <evgen_lyahov@mail.ru>
2021-11-07 15:48:15 +0300

    Example 12

---
+++ 12/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру трёхзначеого числа
+
+int number = new Random().Next(100, 1000);
+int secondNumber = ((number - (number % 10)) % 100);
+int lastNumber = number - secondNumber;
+int removeNumber = secondNumber / 10;
+System.Console.WriteLine("Удаляемая средняя цифра: " + removeNumber);
+System.Console.WriteLine("Случайное число: " + number);
+System.Console.WriteLine("Итоговое число без средней цифры: " + lastNumber);
