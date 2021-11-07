1c23b13ba3af502a74cb8f286c9b8e20a732d488
Evgen <evgen_lyahov@mail.ru>
2021-11-07 09:49:50 +0300

    Example 0

---
+++ 0/Program.cs
@@ -0,0 +1,7 @@
+﻿// 0	Вывести квадрат числа
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int kvadrat;
+kvadrat = number * number;
+System.Console.WriteLine("Вы ввели: " + number);
+System.Console.WriteLine("Квадратное значение этого числа: " + kvadrat);
