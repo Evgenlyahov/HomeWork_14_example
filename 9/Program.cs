1822b3994d4a097014e3de99ca1a13d750311973
Evgen <evgen_lyahov@mail.ru>
2021-11-07 14:25:46 +0300

    Example 9

---
+++ 9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine("Дано трёхзначное число: " + number);
+
+int lostNumber = number % 10;
+System.Console.WriteLine("Последняя цифра трёхзначного числа: " + lostNumber);
