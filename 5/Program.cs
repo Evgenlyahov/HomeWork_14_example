b0613413949caf39beb9ca42292d253af6dba709
Evgen <evgen_lyahov@mail.ru>
2021-11-07 11:13:36 +0300

    Exampe 5

---
+++ 5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления значения функции y=f(a)
+// В моей функции будем находить Y после ввода X и Z. y = x * z
+
+System.Console.Write("Введи X : ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введи Z : ");
+int z = Convert.ToInt32(Console.ReadLine());
+int y = x * z;
+System.Console.WriteLine(x + " * " + z + " = " + y);
