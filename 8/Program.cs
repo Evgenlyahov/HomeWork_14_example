a217f9b6ce7a4fc80f2ecff1771afa2382e4ef79
Evgen <evgen_lyahov@mail.ru>
2021-11-07 14:05:12 +0300

    Example 8

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать чётные числа от 1 до N
+
+int[] array = new int[20];
+Random random = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(random.Next(-100, 100));
+    if(array[i] % 2 == 0 && array[i] > 1)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
