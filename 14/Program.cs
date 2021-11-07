6ce05531318730a9cbf8b0c50dc41f2fbfb13758
Evgen <evgen_lyahov@mail.ru>
2021-11-07 16:12:58 +0300

    Example 14

---
+++ 14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine("Рандомное трёхзначное число: " + number);
+
+int ostatok = (number % 10);
+if (ostatok != 0)
+{
+    System.Console.WriteLine("Третья цифра: " + ostatok);
+}
+else
+{
+    System.Console.WriteLine("Третья цифра равна 0");
+}
