6be5776dac2367b705640dabdf6dc7b0733fcd5e
Evgen <evgen_lyahov@mail.ru>
2021-11-07 14:34:00 +0300

    Example 10

---
+++ 10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine("Дано трёхзначное число: " + number);
+int secondNumber = ((number - number % 10) / 10) % 10; 
+System.Console.WriteLine("Вторая цифра трёхзначного числа: " + secondNumber);
