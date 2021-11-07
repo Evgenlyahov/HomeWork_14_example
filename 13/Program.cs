33c9331bfbd98a2d4a2d3aa51cea4bb90e203b05
Evgen <evgen_lyahov@mail.ru>
2021-11-07 16:03:04 +0300

    Example 13

---
+++ 13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить кратно ли число заданному, если нет – вывести остаток
+
+float number = new Random().Next(10,21);
+System.Console.WriteLine("Введи число от 2 до 4 : ");
+float numberInput = float.Parse(Console.ReadLine());
+
+if(number % numberInput == 0)
+{
+    System.Console.WriteLine("Числа кратны");
+}
+if(number % numberInput != 0)
+{
+    float ostatok = number % numberInput;
+    System.Console.WriteLine("Числа не кратны. Остаток от деления равен: " + ostatok);
+}
+System.Console.WriteLine("Введённое число: " + numberInput);
+System.Console.WriteLine("Рандомное число: " + number);
