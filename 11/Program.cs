feaeed12389757100ec7758edb574e8c9656bfca
Evgen <evgen_lyahov@mail.ru>
2021-11-07 15:26:26 +0300

    Example 11

---
+++ 11/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую чифру числа
+
+int interval = new Random().Next(10, 100);
+System.Console.WriteLine("Случайное число: " + interval);
+
+int b = interval % 10;
+int a = (interval - (interval % 10)) / 10;
+if (a > b)
+{
+    System.Console.WriteLine("Первая цифра: " + a + ", больше второй цифры: " + b);
+}
+if (a == b)
+{
+    System.Console.WriteLine("Цифры равны");
+}
+if (a < b)
+{
+    System.Console.WriteLine("Вторая цифра: " + b + ", больше первой цифры: " + a);
+}
